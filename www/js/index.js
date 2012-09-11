bbf0ac0c0669503a41f16375c7c1e6a484097a5b
Victor H. Salazar <xtealc@gmail.com>
2012-09-11 10:33:27 -0400

    added device ready event

--- www/js/index.js
+++ www/js/index.js
@@ -8,7 +8,7 @@ var app = {
     deviceready: function() {
         // note that this is an event handler so the scope is that of the event
         // so we need to call app.report(), and not this.report()
-        app.report('deviceready');
+        alert('deviceready');
     },
     report: function(id) { 
         console.log("report:" + id);
